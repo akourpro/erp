f671b4e27f6c2f75bc2fe1edfaf7fc30bb27feb2
Mohammad Akour <pro.akour@gmail.com>
2024-03-05 18:32:29 +0300

    دخول الادمن

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- Host: localhost	Database: erp
 -- ------------------------------------------------------
 -- Server version 	10.4.32-MariaDB
--- Date: Tue, 05 Mar 2024 18:31:22 +0300
+-- Date: Tue, 05 Mar 2024 18:32:13 +0300
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -182,4 +182,4 @@ COMMIT;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on: Tue, 05 Mar 2024 18:31:23 +0300
+-- Dump completed on: Tue, 05 Mar 2024 18:32:13 +0300
